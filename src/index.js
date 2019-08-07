cd13e5e96560122b6199276154cb5709a090a238
lgm527 <laurellmccaffrey@gmail.com>
2019-08-07 12:18:18 -0400

    figures out ajax

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const fullname = document.getElementById("fullname");
 console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
-
+function fetchPerson() {
+  fetch("https://randomuser.me/api/")
+    .then(res => res.json())
+    .then(function(json) {
+      // console.log(json.results[0].gender);
+      document.getElementById("street").innerText = json.results[0].location.street;
+      document.getElementById("")
+    });
+}
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+  const button = document.querySelector("button");
+  button.addEventListener("click", function() {
+    fetchPerson()
+  });
 });
